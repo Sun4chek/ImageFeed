6cae94cc57a85059f15e6cdfa8005bc96f99078a
Sun4chek <boysfromhell@mail.ru>
2025-04-09 09:41:22 +0300

    добавил отладку для выведения зума в консоль

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -1,55 +1,40 @@
-//
-//  SingleImageViewController.swift
-//  ImageFeed
-//
-//  Created by Волошин Александр on 06.04.2025.
-//
-
 import UIKit
 
-final class SingleImageViewController: UIViewController{
+final class SingleImageViewController: UIViewController {
     
-    
-    var image : UIImage?{
-        didSet{
-            guard isViewLoaded,let image else { return }
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded, let image = image else { return }
             imageView.image = image
-            
             imageView.frame.size = image.size
             rescaleAndCenterImageInScrollView(image: image)
-            
         }
     }
     
     @IBOutlet private var scrollView: UIScrollView!
-    
-    
     @IBOutlet private var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
+
         
-        
-        guard let image else { return }
+        guard let image = image else { return }
         imageView.image = image
         imageView.frame.size = image.size
         rescaleAndCenterImageInScrollView(image: image)
-        
-        
-        
-        
     }
+    
     @IBAction func didTapShareButton(_ sender: UIButton) {
-        guard let image else { return }
+        guard let image = image else { return }
         let shareController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         present(shareController, animated: true)
     }
+    
     @IBAction func didTapBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
         let minZoomScale = scrollView.minimumZoomScale
@@ -63,27 +48,19 @@ final class SingleImageViewController: UIViewController{
         scrollView.setZoomScale(scale, animated: false)
         scrollView.layoutIfNeeded()
         centerImage()
-        
-        
-        
-        
-        
-        
+        print("Initial zoom scale: \(scrollView.zoomScale)") // Отладка
     }
-    
     
     func centerImage() {
         let boundsSize = scrollView.bounds.size
         var frameToCenter = imageView.frame
         
-        // Центрирование по горизонтали
         if frameToCenter.size.width < boundsSize.width {
             frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2
         } else {
             frameToCenter.origin.x = 0
         }
         
-        // Центрирование по вертикали
         if frameToCenter.size.height < boundsSize.height {
             frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2
         } else {
@@ -94,13 +71,18 @@ final class SingleImageViewController: UIViewController{
     }
 }
 
-
 extension SingleImageViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
     
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerImage()
+        print("Zoom scale during zooming: \(scrollView.zoomScale)") // Отладка
+    }
+    
     func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
-            centerImage()
+        centerImage()
+        print("Zoom scale after zooming: \(scrollView.zoomScale)") // Отладка
     }
 }
